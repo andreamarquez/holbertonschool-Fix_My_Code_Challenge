dfb4935d5a9c8066f4b55f5d3575f9184d297568
andreamarquez <andreamarquezg.95@gmail.com>
2025-05-14 17:06:17 +0200

    remove

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -50,7 +50,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	else
 	{
 		/* Update the previous node's next pointer */
-		(*head)->prev->next = (*head)->next;  // FIX APPLIED HERE
+		(*head)->prev->next = (*head)->next;
 
 		/* Update the next node's prev pointer, if it exists */
 		if ((*head)->next != NULL)
